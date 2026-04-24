2832c54d4d0cf85e8d03df8abde5c16a757ad98b
wonnacottdarren-flipid <wonnacott.darren@gmail.com>
2026-04-24 20:03:08 +0100

    Update consoleFiltersV2.js

--- engines/consoleFiltersV2.js
+++ engines/consoleFiltersV2.js
@@ -1,10 +1,10 @@
 import { matchPs5ConsoleFamily } from "./ps5ConsoleMatch.js";
 
-export function matchesConsoleFamily({ item, queryContext }) {
+export function matchesConsoleFamily({ text = "", item, queryContext } = {}) {
   const family = String(queryContext?.family || "");
 
   if (family.startsWith("ps5")) {
-    return matchPs5ConsoleFamily({ item, queryContext });
+    return matchPs5ConsoleFamily({ text, item, queryContext });
   }
 
   return true;
